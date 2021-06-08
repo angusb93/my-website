04c9677810dd4af10d7b8475fd12e122988111f9
Angus <angusbuick@gmail.com>
2021-06-08 15:47:16 +1000

    added headline and background photo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,28 @@
-import { Canvas } from "@react-three/fiber";
-import { Suspense } from "react";
-import Model from "./model";
+import React from "react";
+import "@fontsource/noto-sans-jp";
+import andy from "./andy.png";
 
-function App() {
+const pageStyles = {
+  color: "#232129",
+  backgroundImage: `url(${andy})`,
+  backgroundSize: "cover",
+  backgroundPosition: "top",
+  height: "100vh",
+  position: "relative",
+  paddingRight: "400px",
+  fontFamily: "Noto Sans JP",
+  float: "right",
+};
+
+const headingStyles = {
+  fontSize: "100px",
+};
+const App = () => {
   return (
-    <Canvas>
-      <Suspense fallback={null}>
-        <Model />
-      </Suspense>
-    </Canvas>
+    <main style={pageStyles}>
+      <h1 style={headingStyles}>ANGUS BUICK</h1>
+    </main>
   );
-}
+};
+
 export default App;
