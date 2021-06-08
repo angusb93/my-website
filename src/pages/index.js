670e56115f3a574ce4fb128edfecc77048dbd771
Angus <angusbuick@gmail.com>
2021-06-08 16:18:18 +1000

    added staggered headline

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,22 +5,32 @@ import andy from "./andy.png";
 const pageStyles = {
   color: "#232129",
   backgroundImage: `url(${andy})`,
-  backgroundSize: "cover",
+  backgroundSize: "contain",
   backgroundPosition: "top",
-  height: "100vh",
+  backgroundRepeat: "no-repeat",
+  height: "70vh",
   position: "relative",
-  paddingRight: "400px",
   fontFamily: "Noto Sans JP",
-  float: "right",
 };
 
 const headingStyles = {
-  fontSize: "100px",
+  fontSize: "8rem",
+  float: "left",
+};
+const angusStyles = {
+  paddingLeft: "150px",
+};
+const buickStyles = {
+  paddingLeft: "240px",
 };
 const App = () => {
   return (
     <main style={pageStyles}>
-      <h1 style={headingStyles}>ANGUS BUICK</h1>
+      <h1 style={headingStyles}>
+        <span style={angusStyles}>ANGUS </span>
+        <br />
+        <span style={buickStyles}>BUICK</span>
+      </h1>
     </main>
   );
 };
