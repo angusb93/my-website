b8352062c2d27231157836de34d26ee33d99fe40
angusb93 <angusbuick@gmail.com>
2021-07-27 16:08:27 +1000

    added texture to index instead of heading for the whole thing

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -2,7 +2,6 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { SocialIcon } from "react-social-icons";
 import andy from "./andy.png";
-import background from "./background.png";
 // import "./noise.css";
 const useStyles = makeStyles({
   root: {
@@ -11,55 +10,6 @@ const useStyles = makeStyles({
     width: "100%",
     overflow: "hidden",
     background: "rgb(240,240,240)",
-  },
-  "@-webkit-keyframes noise": {
-    "0%": { transform: "translate3d(0, 9rem, 0)" },
-    "10%": { transform: "translate3d(-1rem, -4rem, 0)" },
-    "20%": { transform: "translate3d(-8rem, 2rem, 0)" },
-    "30%": { transform: "translate3d(9rem, -9rem, 0)" },
-    "40%": { transform: "translate3d(-2rem, 7rem, 0)" },
-    "50%": { transform: "translate3d(-9rem, -4rem, 0)" },
-    "60%": { transform: "translate3d(2rem, 6rem, 0)" },
-    "70%": { transform: "translate3d(7rem, -8rem, 0)" },
-    "80%": { transform: "translate3d(-9rem, 1rem, 0)" },
-    "90%": { transform: "translate3d(6rem, -5rem, 0)" },
-    to: { transform: "translate3d(-7rem, 0, 0)" },
-  },
-  "@keyframes noise": {
-    "0%": { transform: "translate3d(0, 9rem, 0)" },
-    "10%": { transform: "translate3d(-1rem, -4rem, 0)" },
-    "20%": { transform: "translate3d(-8rem, 2rem, 0)" },
-    "30%": { transform: "translate3d(9rem, -9rem, 0)" },
-    "40%": { transform: "translate3d(-2rem, 7rem, 0)" },
-    "50%": { transform: "translate3d(-9rem, -4rem, 0)" },
-    "60%": { transform: "translate3d(2rem, 6rem, 0)" },
-    "70%": { transform: "translate3d(7rem, -8rem, 0)" },
-    "80%": { transform: "translate3d(-9rem, 1rem, 0)" },
-    "90%": { transform: "translate3d(6rem, -5rem, 0)" },
-    to: { transform: "translate3d(-7rem, 0, 0)" },
-  },
-  noise: {
-    width: "100%",
-    height: "100%",
-    position: "fixed",
-    left: "0",
-    top: "0",
-    pointerEvents: "none",
-    zIndex: "1",
-
-    "&:after": {
-      content: '""',
-      width: "calc(100% + 20rem)",
-      height: "calc(100% + 20rem)",
-      backgroundImage: `url(${background})`,
-      backgroundPosition: "50%",
-      position: "absolute",
-      left: "-10rem",
-      top: "-10rem",
-      willChange: "transform",
-      WebkitAnimation: "$noise 1s steps(2) infinite",
-      animation: "$noise 1s steps(2) infinite",
-    },
   },
 
   pageStyles: {
@@ -128,7 +78,6 @@ const Heading = () => {
           />
         </h2>
       </div>
-      <div className={classes.noise}></div>
     </div>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Heading from "../components/Heading";
 import Body from "../components/Body";
 import Projects from "../components/Projects";
 import { withStyles } from "@material-ui/core/styles";
+import background from "./background.png";
 
 import ReactPageScroller from "react-page-scroller";
 const styles = {
@@ -23,6 +24,55 @@ const styles = {
     "html, body": {
       margin: 0,
       padding: 0,
+    },
+  },
+  "@-webkit-keyframes noise": {
+    "0%": { transform: "translate3d(0, 9rem, 0)" },
+    "10%": { transform: "translate3d(-1rem, -4rem, 0)" },
+    "20%": { transform: "translate3d(-8rem, 2rem, 0)" },
+    "30%": { transform: "translate3d(9rem, -9rem, 0)" },
+    "40%": { transform: "translate3d(-2rem, 7rem, 0)" },
+    "50%": { transform: "translate3d(-9rem, -4rem, 0)" },
+    "60%": { transform: "translate3d(2rem, 6rem, 0)" },
+    "70%": { transform: "translate3d(7rem, -8rem, 0)" },
+    "80%": { transform: "translate3d(-9rem, 1rem, 0)" },
+    "90%": { transform: "translate3d(6rem, -5rem, 0)" },
+    to: { transform: "translate3d(-7rem, 0, 0)" },
+  },
+  "@keyframes noise": {
+    "0%": { transform: "translate3d(0, 9rem, 0)" },
+    "10%": { transform: "translate3d(-1rem, -4rem, 0)" },
+    "20%": { transform: "translate3d(-8rem, 2rem, 0)" },
+    "30%": { transform: "translate3d(9rem, -9rem, 0)" },
+    "40%": { transform: "translate3d(-2rem, 7rem, 0)" },
+    "50%": { transform: "translate3d(-9rem, -4rem, 0)" },
+    "60%": { transform: "translate3d(2rem, 6rem, 0)" },
+    "70%": { transform: "translate3d(7rem, -8rem, 0)" },
+    "80%": { transform: "translate3d(-9rem, 1rem, 0)" },
+    "90%": { transform: "translate3d(6rem, -5rem, 0)" },
+    to: { transform: "translate3d(-7rem, 0, 0)" },
+  },
+  noise: {
+    width: "100%",
+    height: "100%",
+    position: "fixed",
+    left: "0",
+    top: "0",
+    pointerEvents: "none",
+    zIndex: "1",
+
+    "&:after": {
+      content: '""',
+      width: "calc(100% + 20rem)",
+      height: "calc(100% + 20rem)",
+      backgroundImage: `url(${background})`,
+      backgroundPosition: "50%",
+      position: "absolute",
+      left: "-10rem",
+      top: "-10rem",
+      willChange: "transform",
+      WebkitAnimation: "$noise 1s steps(2) infinite",
+      animation: "$noise 1s steps(2) infinite",
     },
   },
 };
@@ -56,6 +106,8 @@ class App extends Component {
             <Body />
             <Projects />
           </ReactPageScroller>
+          <div />
+          <div className={classes.noise}></div>
         </main>
       </React.Fragment>
     );
